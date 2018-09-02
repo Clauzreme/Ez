c81b8248953eb80915ea8abb2f4977ab426afeea
Mikhail Stepanov <mikestepanov777@gmail.com>
2018-09-02 01:54:53 -0700

    list refactored for singular

--- src/Components/mui/List.js
+++ src/Components/mui/List.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
 import { withStyles } from '@material-ui/core/styles'
 import ListSubheader from '@material-ui/core/ListSubheader'
@@ -13,6 +13,23 @@ import SendIcon from '@material-ui/icons/Send'
 import ExpandLess from '@material-ui/icons/ExpandLess'
 import ExpandMore from '@material-ui/icons/ExpandMore'
 import StarBorder from '@material-ui/icons/StarBorder'
+
+import { Link } from 'react-router-dom'
+
+const listItems = {
+  'Built-in Types': {
+    String: '/types/string',
+    Digit: '/types/digit',
+    Float: '/types/float',
+    Boolean: '/types/boolean',
+    Undefined: '/types/undefined',
+    Null: '/types/null',
+    Array: '/types/array',
+    Object: '/types/object',
+    Set: '/types/set',
+  },
+  Arithmetics: '/arithmetics',
+}
 
 const styles = theme => ({
   root: {
@@ -37,17 +54,35 @@ class NestedList extends React.Component {
     return (
       <div className={classes.root}>
         <List component="nav">
-          <ListItem button onClick={this.handleClick}>
-            <ListItemText inset primary="Inbox" />
-            {this.state.open ? <ExpandLess /> : <ExpandMore />}
-          </ListItem>
-          <Collapse in={this.state.open} timeout="auto" unmountOnExit>
-            <List component="div" disablePadding>
-              <ListItem button>
-                <ListItemText inset primary="Starred" />
-              </ListItem>
-            </List>
-          </Collapse>
+          {Object.keys(listItems).map(key => {
+            const val = listItems[key]
+            if (typeof val === 'string') {
+              return (
+                <Link to={val}>
+                  <ListItem button>
+                    <ListItemText primary={key} />
+                  </ListItem>
+                </Link>
+              )
+            }
+            return (
+              <Fragment key={key}>
+                <ListItem button onClick={this.handleClick}>
+                  <ListItemText primary={key} />
+                  {this.state.open ? <ExpandLess /> : <ExpandMore />}
+                </ListItem>
+                <Collapse in={this.state.open} timeout="auto" unmountOnExit>
+                  <List component="div" disablePadding>
+                    {Object.keys(listItems[key]).map(key2 => (
+                      <ListItem button key={key2}>
+                        <ListItemText primary={key2} />
+                      </ListItem>
+                    ))}
+                  </List>
+                </Collapse>
+              </Fragment>
+            )
+          })}
         </List>
       </div>
     )
