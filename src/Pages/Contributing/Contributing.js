8c312d12a15e2ca4a27ee21b7def398f9acba8d4
Mikhail Stepanov <mikestepanov777@gmail.com>
2018-09-04 23:55:10 -0700

    contributing page mvp

--- src/Pages/Contributing/Contributing.js
+++ src/Pages/Contributing/Contributing.js
@@ -2,8 +2,10 @@ import React, { Component, Fragment } from 'react'
 import { compose } from 'redux'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 
 import Header from '../../Components/Header/Header'
+import PaperContainer from '../../Components/mui/PaperContainer'
 
 import withContainer from '../../Components/withContainer'
 
@@ -15,6 +17,34 @@ class Contributing extends Component {
     return (
       <Fragment>
         <Header headerName={headerName} />
+        <PaperContainer>
+          <h2>
+            I would love to hear any{' '}
+            <Link to={'/feedback'} className="inlineLink">
+              feedback
+            </Link>{' '}
+            or{' '}
+            <a
+              href="https://github.com/EzLang/Ez/pulls"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="inlineLink"
+            >
+              contributing
+            </a>{' '}
+            you make. :)
+          </h2>
+          <h3>
+            <a
+              href="https://help.github.com/articles/about-pull-requests/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="inlineLink"
+            >
+              About pull requests
+            </a>
+          </h3>
+        </PaperContainer>
       </Fragment>
     )
   }
